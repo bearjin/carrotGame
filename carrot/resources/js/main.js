1c801a914f0749a7a97791f781981479948b68fd
bearjin <kwj5030@gmail.com>
2021-07-06 14:46:22 +0900

    게임 소스 업데이트

--- carrot/resources/js/main.js
+++ carrot/resources/js/main.js
@@ -18,66 +18,69 @@ class gameSetting {
         this.currentTime = gameTime;
         this.timerInterval;
         this.score;
+        this.targetSize = 80;
         this.bgAudio = new Audio('./resources/sound/bg.mp3');
         this.winAudio = new Audio('./resources/sound/game_win.mp3');
         this.carrotAudio = new Audio('./resources/sound/carrot_pull.mp3');
         this.bugAudio = new Audio('./resources/sound/bug_pull.mp3');
+        this.alertAudio = new Audio('./resources/sound/alert.wav');
 
-        this.onEvent();
-    }
-
-    /**
-     * 게임 이벤트 등록
-     */
-    onEvent() {
         this.$playBtn.addEventListener('click', event => {
             const state = event.target.dataset.state;
 
             if (state === 'start') {
                 this.pauseGame();
-                this.bgAudio.pause();
+                this.stopSound(this.bgAudio);
             } else if (state === 'pause') {
                 this.reStartGame();
-                this.bgAudio.play();
+                this.playSound(this.bgAudio);
             } else if (state === 'init') {
                 this.playGame();
-                this.bgAudio.play();
+                this.playSound(this.bgAudio);
             }
         });
+
         this.$replayBtn.addEventListener('click', () => {
             this.initGame();
         });
+
         this.$area.addEventListener('click', event => {
             const $target = event.target;
             if ($target.className === 'bug') {
-                this.bugAudio.play();
+                this.playSound(this.bugAudio);
                 this.failGame();
             } else if ($target.className === 'carrot') {
-                this.carrotAudio.play();
+                this.playSound(this.carrotAudio);
             }
             this.removeTarget($target);
             this.checkScore();
             if (this.score === 0) {
                 this.successGame();
-                this.bgAudio.pause();
-                this.winAudio.play();
+                this.stopSound(this.bgAudio);
+                this.playSound(this.winAudio);
             }
         });
+    }
+
+    /**
+     * 게임세팅 초기화
+     */
+    resetSetting() {
+        this.$minTxt.innerText = '00';
+        this.$secTxt.innerText = '00';
+        this.$score.innerText = 0;
+        this.currentTime = this.gameTime;
+        this.$area.innerHTML = '';
     }
 
     /**
      * 게임 재설정
      */
     initGame() {
-        clearInterval(this.timerInterval);
-        this.$result.style.visibility = 'hidden';
-        this.$minTxt.innerText = '00';
-        this.$secTxt.innerText = '00';
-        this.$score.innerText = 0;
-        this.currentTime = this.gameTime;
-        this.$area.innerHTML = '';
-        this.$playBtn.classList.remove('fa-stop');
-        this.$playBtn.setAttribute('data-state', 'init');
+        this.stopTimer();
+        this.hidePopUp();
+        this.resetSetting();
+        this.changeButton('init');
     }
 
     /**
@@ -87,9 +90,58 @@ class gameSetting {
         this.startTimer();
         this.makeTarget();
         this.checkScore();
-        this.$result.style.visibility = 'hidden';
-        this.$playBtn.classList.add('fa-stop');
-        this.$playBtn.setAttribute('data-state', 'start');
+        this.hidePopUp();
+        this.changeButton('start');
+    }
+
+    /**
+     * 게임 성공
+     */
+    successGame() {
+        this.stopTimer();
+        this.changeButton('init');
+        this.showPopUp("YOU WIN", "fa-laugh-squint");
+    }
+
+    /**
+     * 게임 실패
+     */
+    failGame() {
+        this.stopSound(this.bgAudio);
+        this.stopTimer();
+        this.changeButton('init');
+        this.showPopUp("YOU LOSE", "fa-sad-tear");
+    }
+
+    /**
+     * 게임 일시정지
+     */
+    pauseGame() {
+        this.stopTimer();
+        this.changeButton('pause');
+        this.showPopUp("RESTART?", "fa-surprise");
+    }
+
+    /**
+     * 게임 재시작
+     */
+    reStartGame() {
+        this.startTimer();
+        this.changeButton('start');
+        this.hidePopUp();
+    }
+
+    /**
+     * 버튼 변경하기
+     * @param {string} state 버튼 상태값 
+     */
+    changeButton(state) {
+        if (state === 'start') {
+            this.$playBtn.classList.add('fa-stop');
+        } else {
+            this.$playBtn.classList.remove('fa-stop');
+        }
+        this.$playBtn.setAttribute('data-state', state);
     }
 
     /**
@@ -97,25 +149,34 @@ class gameSetting {
      */
     startTimer() {
         let time = this.gameTime;
-        let min = parseInt(time / 60);
-        let sec = time % 60;
-        min < 10 ? this.$minTxt.innerText = `0${min}` : this.$minTxt.innerText = `${min}`;
-        sec < 10 ? this.$secTxt.innerText = `0${sec}` : this.$secTxt.innerText = `${sec}`;
-
-        clearInterval(this.timerInterval);
+        this.updateTimerText(time);
         this.timerInterval = setInterval(() => {
-            this.currentTime = time--;
-            if (time < 0) {
-                clearInterval(this.timerInterval);
+            if (time <= 0) {
+                this.stopTimer();
                 this.failGame();
-                this.bgAudio.pause();
+                this.stopSound(this.bgAudio);
                 return;
             }
-            min = parseInt(time / 60);
-            sec = time % 60;
-            min < 10 ? this.$minTxt.innerText = `0${min}` : this.$minTxt.innerText = `${min}`;
-            sec < 10 ? this.$secTxt.innerText = `0${sec}` : this.$secTxt.innerText = `${sec}`;
+            this.updateTimerText(--time);
         }, 1000);
+    }
+
+    /**
+     * 타이머 멈추기
+     */
+    stopTimer() {
+        clearInterval(this.timerInterval);
+    }
+
+    /**
+     * 타이머 시간 업데이트
+     * @param {string} time 현재 시간
+     */
+    updateTimerText(time) {
+        const min = Math.floor(time / 60);
+        const sec = time % 60;
+        min < 10 ? this.$minTxt.innerText = `0${min}` : this.$minTxt.innerText = `${min}`;
+        sec < 10 ? this.$secTxt.innerText = `0${sec}` : this.$secTxt.innerText = `${sec}`;
     }
 
     /**
@@ -138,8 +199,8 @@ class gameSetting {
 
         for (let i = 0; i < this.targetNum; i++) {
             const randomNum = Math.floor(Math.random() * 2);
-            const randomPositionX = Math.floor(Math.random() * (areaSize.width - 80));
-            const randomPositionY = Math.floor(Math.random() * (areaSize.height - 80));
+            const randomPositionX = Math.floor(Math.random() * (areaSize.width - this.targetSize));
+            const randomPositionY = Math.floor(Math.random() * (areaSize.height - this.targetSize));
 
             if (randomNum === 0) {
                 img += `
@@ -156,7 +217,7 @@ class gameSetting {
 
     /**
      * 타겟 제거하기
-     * @param {element} target 
+     * @param {element} target 타겟
      */
     removeTarget(target) {
         if (target.className === 'carrot') {
@@ -165,72 +226,40 @@ class gameSetting {
     }
 
     /**
-     * 게임 성공
+     * 팝업 쇼
+     * @param {string} text 팝업 문구
+     * @param {string} icon 아이콘 클래스 
      */
-    successGame() {
-        clearInterval(this.timerInterval);
+    showPopUp(text, icon) {
         this.$resultTxt.innerHTML = `
-            <span class="result_txt">YOU WIN</span>
-            <i class="fas fa-laugh-squint"></i>
+            <span class="result_txt">${text}</span>
+            <i class="fas ${icon}"></i>
         `;
         this.$result.style.visibility = 'visible';
-        this.$playBtn.classList.remove('fa-stop');
-        this.$playBtn.setAttribute('data-state', 'init');
     }
 
     /**
-     * 게임 실패
+     * 팝업 히든
      */
-    failGame() {
-        clearInterval(this.timerInterval);
-        this.$resultTxt.innerHTML = `
-            <span class="result_txt">YOU LOSE</span>
-            <i class="fas fa-sad-tear"></i>
-        `;
-        this.$result.style.visibility = 'visible';
-        this.$playBtn.classList.remove('fa-stop');
-        this.$playBtn.setAttribute('data-state', 'init');
-        this.bgAudio.pause();
-    }
-
-    /**
-     * 게임 일시정지
-     */
-    pauseGame() {
-        clearInterval(this.timerInterval);
-        this.$resultTxt.innerHTML = `
-        <span class="result_txt">RESTART?</span>
-        <i class="fas fa-surprise"></i>
-        `;
-        this.$result.style.visibility = 'visible';
-        this.$playBtn.classList.remove('fa-stop');
-        this.$playBtn.setAttribute('data-state', 'pause');
-    }
-
-    /**
-     * 게임 재시작
-     */
-    reStartGame() {
+    hidePopUp() {
         this.$result.style.visibility = 'hidden';
-        this.$playBtn.classList.add('fa-stop');
-        this.$playBtn.setAttribute('data-state', 'start');
+    }
 
-        let time = this.currentTime;
-        let min = parseInt(time / 60);
-        let sec = time % 60;
+    /**
+     * 사운드 시작
+     * @param {element} sound 플레이할 사운드
+     */
+    playSound(sound) {
+        sound.currentTime = 0;
+        sound.play();
+    }
 
-        this.timerInterval = setInterval(() => {
-            this.currentTime = time--;
-            if (time < 0) {
-                clearInterval(this.timerInterval);
-                this.failGame();
-                return;
-            }
-            min = parseInt(time / 60);
-            sec = time % 60;
-            min < 10 ? this.$minTxt.innerText = `0${min}` : this.$minTxt.innerText = `${min}`;
-            sec < 10 ? this.$secTxt.innerText = `0${sec}` : this.$secTxt.innerText = `${sec}`;
-        }, 1000);
+    /**
+     * 사운드 멈춤
+     * @param {element} sound 플레이할 사운드
+     */
+    stopSound(sound) {
+        sound.pause();
     }
 }
 
